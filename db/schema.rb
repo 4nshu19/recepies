ba10a685791a5ee8df09245b3cf5efeeaf3d20eb
anshu19 <4nshu19@gmail.com>
2016-01-07 17:50:47 +0000

    added chef table and model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160107135404) do
+ActiveRecord::Schema.define(version: 20160107140645) do
+
+  create_table "chefs", force: :cascade do |t|
+    t.string   "chefname"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "recepies", force: :cascade do |t|
     t.string   "name"

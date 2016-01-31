435de57064e2df0cb2abcd47bb30cf8b90175767
anshu19 <4nshu19@gmail.com>
2016-01-31 14:37:07 +0000

    added relations of chef,recepie

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -1,3 +1,9 @@
 class Chef < ActiveRecord::Base
-    validates :chefname, presence: true, lengthh: {minimum: 3, maximum: 40 }
+    has_many :recepies
+    before_save { self.email = email.downcase }
+    validates :chefname, presence: true, length: { minimum: 3, maximum: 40 }
+    #validates :email, presence: true, length: {maximum: 105},
+    #                                    uniqueness: { case-sensitive: false},
+    #                                    format: {with: VALID_EMAIL_REGEX }
+    
 end    
